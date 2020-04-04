2fc424c4f941685a793a197563e0125da81f6524
Krupal Shah <shahkrupal001@gmail.com>
2020-04-04 10:45:15 +0530

    new push

--- app/src/main/java/com/everfino/everfinouser/Fragment/ScanFragment.java
+++ app/src/main/java/com/everfino/everfinouser/Fragment/ScanFragment.java
@@ -1,21 +1,37 @@
 package com.everfino.everfinouser.Fragment;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.everfino.everfinouser.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+
+import java.util.Collections;
+import java.util.List;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ScanFragment extends Fragment {
 
+    ZXingScannerView qrCodeScanner;
+    Boolean isCameraOn = false;
 
     public ScanFragment() {
         // Required empty public constructor
@@ -25,8 +41,40 @@ public class ScanFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan, container, false);
+        // Inflate the layout for this fragment4
+        View view = inflater.inflate(R.layout.fragment_scan, container, false);
+        qrCodeScanner = view.findViewById(R.id.qrCodeScanner);
+
+        Log.e("####", "OnCreate");
+        setScannerProperties();
+        qrCodeScanner.startCamera();
+        isCameraOn = true;
+        qrCodeScanner.setResultHandler(new ZXingScannerView.ResultHandler() {
+            @Override
+            public void handleResult(Result result) {
+
+                Toast.makeText(getActivity(), "" + result, Toast.LENGTH_SHORT).show();
+                Fragment fragment=new HomeFragment();
+                loadFragment(fragment);
+            }
+        });
+        return view;
     }
 
+
+    private void setScannerProperties() {
+        qrCodeScanner.setFormats(Collections.singletonList((BarcodeFormat.QR_CODE)));
+        qrCodeScanner.setAutoFocus(true);
+        qrCodeScanner.setLaserColor(R.color.colorAccent);
+        qrCodeScanner.setMaskColor(R.color.colorAccent);
+
+    }
+
+    public void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
